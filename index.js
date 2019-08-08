f1807d97286671219ac9b818c86082c71ab687cc
jackbolton901 <bltnjck@gmail.com>
2019-08-08 12:30:47 -0500

    am classwork

---
+++ index.js
@@ -0,0 +1,18 @@
+// Problem1
+for(var x= -4;x<9;x= x+1) {
+    if (x >= -3 && x <= 8) {
+        console.log(x)
+    }
+}
+
+//problem 2
+for(var t= 0;t<51;t=t+1) {if(t===10){
+    console.log("TEN")
+}
+else console.log(t)
+}
+
+//problem3
+
+var num= parseInt(prompt("Enter number"))
+var num2= parseInt(prompt("Enter number"))
